8239248a8bf6af03cffbee4c868c4f70a7f5ea07
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2014-02-27 22:07:05 -0500

    No inyectar el service container en la clase ThemeDoctrineCrud

--- Theme/ThemeDoctrineCrud.php
+++ Theme/ThemeDoctrineCrud.php
@@ -11,26 +11,32 @@
 
 namespace CULabs\AdminBundle\Theme;
 
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Sensio\Bundle\GeneratorBundle\Generator\DoctrineFormGenerator;
 use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
 use CULabs\AdminBundle\Generator\DoctrineFiterGenerator;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpKernel\KernelInterface;
 
-class ThemeDoctrineCrud implements ThemeDoctrineCrudInterface, ContainerAwareInterface
+class ThemeDoctrineCrud implements ThemeDoctrineCrudInterface
 {
-    protected $generator;    
-    protected $container;
+    protected $generator;
     protected $theme_name;
     protected $formGenerator;
     protected $filterFormGenerator;
+    protected $filesystem;
+    protected $kernel;
 
-    public function __construct(ContainerInterface $container, $theme_name)
+    public function __construct(KernelInterface $kernel, Filesystem $filesystem, $theme_name)
     {
-        $this->setContainer($container);
+        $this->filesystem = $filesystem;
         $this->theme_name = $theme_name;
+        $this->kernel     = $kernel;
     }
-    
+
+    /**
+     * @return DoctrineCrudGenerator
+     * @throws \Symfony\Component\DependencyInjection\Exception\InvalidArgumentException
+     */
     public function getGenerator() 
     {   
         if (null === $this->generator) {
@@ -39,12 +45,16 @@ class ThemeDoctrineCrud implements ThemeDoctrineCrudInterface, ContainerAwareInt
             if (!file_exists($dir_path))
                 throw new InvalidArgumentException(sprintf('%s is no directory', $dir_path));
             
-            $this->generator = new DoctrineCrudGenerator($this->getContainer()->get('filesystem'), $dir_path);
+            $this->generator = new DoctrineCrudGenerator($this->filesystem, $dir_path);
         }
 
         return $this->generator;
     }
-    
+
+    /**
+     * @return DoctrineFormGenerator
+     * @throws \Symfony\Component\DependencyInjection\Exception\InvalidArgumentException
+     */
     public function getFormGenerator() 
     {   
         if (null === $this->formGenerator) {
@@ -53,12 +63,17 @@ class ThemeDoctrineCrud implements ThemeDoctrineCrudInterface, ContainerAwareInt
             if (!file_exists($dir_path))
                 throw new InvalidArgumentException(sprintf('%s is no directory', $dir_path));
             
-            $this->formGenerator = new DoctrineFormGenerator($this->getContainer()->get('filesystem'));
+            $this->formGenerator = new DoctrineFormGenerator($this->filesystem);
             $this->formGenerator->setSkeletonDirs($this->skeletonPath());
         }
 
         return $this->formGenerator;
     }
+
+    /**
+     * @return DoctrineFiterGenerator
+     * @throws \Symfony\Component\DependencyInjection\Exception\InvalidArgumentException
+     */
     public function getFilterFormGenerator()
     {   
         if (null === $this->filterFormGenerator) {
@@ -67,33 +82,28 @@ class ThemeDoctrineCrud implements ThemeDoctrineCrudInterface, ContainerAwareInt
             if (!file_exists($dir_path))
                 throw new InvalidArgumentException(sprintf('%s is no directory', $dir_path));
             
-            $this->filterFormGenerator = new DoctrineFiterGenerator($this->getContainer()->get('filesystem'), $dir_path);
+            $this->filterFormGenerator = new DoctrineFiterGenerator($this->filesystem, $dir_path);
         }
 
         return $this->filterFormGenerator;
     }
-    
+
+    /**
+     * @return string
+     * @throws \Symfony\Component\DependencyInjection\Exception\InvalidArgumentException
+     */
     protected function skeletonPath()
     {
         $part_name = explode(':', $this->theme_name);        
         if (count($part_name) != 2)
             throw new InvalidArgumentException('The thene name most similar to CULabsAdminBundle:default');
         
-        $bundle = $this->getContainer()->get('kernel')->getBundle($part_name[0]);
+        $bundle = $this->kernel->getBundle($part_name[0]);
         
         $dir_path = $bundle->getPath().sprintf('/Resources/skeleton/%s/',$part_name[1]);        
         if (!file_exists($dir_path))
             throw new InvalidArgumentException(sprintf('%s is no directory', $dir_path));
         
         return $dir_path;
-    }
-    
-    public function setContainer(ContainerInterface $container = null)
-    {
-        $this->container = $container;
-    }
-    public function getContainer()
-    {
-        return $this->container;
     }
 }
