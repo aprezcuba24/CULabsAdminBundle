6b5d65118fbd6fa27a4fa47a498bbb6075eb2e04
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2014-05-31 09:38:57 -0400

    Serializar los filtros de entidad

--- Controller/CRUDController.php
+++ Controller/CRUDController.php
@@ -3,6 +3,7 @@
 namespace CULabs\AdminBundle\Controller;
 
 use CULabs\AdminBundle\Controller\Controller as BaseController;
+use Symfony\Component\Form\Form;
 
 /**
  * Description of Controller
@@ -15,22 +16,47 @@ class CRUDController extends BaseController
     {
         return $this->getSession('filters', array());
     }
+
     protected function setFilters(array $filters)
     {
+        foreach ($filters as $key => $item) {
+            if (is_object($item) && is_callable(array($item, 'getId'))) {
+                $filters[$key] = $item->getId();
+            }
+        }
+
         $this->setSession('filters', $filters);
     }
+
+    protected function setFilterInForm(Form $form)
+    {
+        $filters = $this->getFilters();
+        foreach ($form->all() as $child) {
+            if ($child->getConfig()->getType()->getInnerType()->getName() == 'filter_entity' && isset($filters[$child->getName()])) {
+                $name = $child->getName();
+                $class = $child->getConfig()->getOption('class');
+                $entity = $this->getRepository($class)->find($filters[$name]);
+                $filters[$name] = $entity;
+            }
+        }
+        $form->setData($filters);
+    }
+
     protected function getSort()
     {
         return $this->getSession('sort', array());
     }
+
     protected function setSort($sort)
     {
         $this->setSession('sort', $sort);
     }
+
     protected function getPage()
     {
         return $this->getSession('page', 1);
     }
+
     protected function setPage($page)
     {
         $this->setSession('page', $page);
@@ -39,6 +65,7 @@ class CRUDController extends BaseController
     {
         return $this->getSession('maxperpage', $this->container->getParameter('culabs.admin.list_cant'));
     }
+
     protected function setMaxPerPage($maxperpage)
     {
         $this->setSession('maxperpage', $maxperpage);
