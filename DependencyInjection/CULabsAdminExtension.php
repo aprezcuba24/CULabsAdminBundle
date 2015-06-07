7b00e73686ae8491ffe58caecda291472401ed17
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2015-06-07 14:30:06 -0400

    Evitar el error al usuar factory-method deprecate en symfony 2.7

--- DependencyInjection/CULabsAdminExtension.php
+++ DependencyInjection/CULabsAdminExtension.php
@@ -7,6 +7,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -26,6 +27,9 @@ class CULabsAdminExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+
+        $loaderYml = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loaderYml->load('services.yml');
 
         $this->complileThemeColletions($container);
 
